22f96bad6c71525eba3d65003c2101b861c6e177
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-11 17:06:00 +0530

    logo

---
+++ fun.js
@@ -0,0 +1,8 @@
+const array = [
+    {que:"Q1.The brain of any computer system", A:"ALU", B:"Memory", C:"Control Unit", D:"CPU"}
+];
+document.getElementById('que').innerHTML=array[0].que;
+document.getElementById('a').innerHTML=array[0].A;
+document.getElementById('b').innerHTML=array[0].B;
+document.getElementById('c').innerHTML=array[0].C;
+document.getElementById('d').innerHTML=array[0].D;
