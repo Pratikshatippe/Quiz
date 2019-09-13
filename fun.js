a592b5fc8349465ac6c7f46783d2e3a5fa7ba0d9
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-13 13:06:18 +0530

    updated

--- fun.js
+++ fun.js
@@ -1,8 +1,21 @@
-const array = [
-    {que:"Q1.The brain of any computer system", A:"ALU", B:"Memory", C:"Control Unit", D:"CPU"}
-];
-document.getElementById('que').innerHTML=array[0].que;
-document.getElementById('a').innerHTML=array[0].A;
-document.getElementById('b').innerHTML=array[0].B;
-document.getElementById('c').innerHTML=array[0].C;
-document.getElementById('d').innerHTML=array[0].D;
+// const array = [
+//     {que:"Q1.The brain of any computer system", A:"ALU", B:"Memory", C:"Control Unit", D:"CPU"}
+// ];
+// document.getElementsByClassName('que').innerHTML=array[0].que;
+// document.getElementsById('a').innerHTML=array[0].A;
+// document.getElementById('b').innerHTML=array[0].B;
+// document.getElementById('c').innerHTML=array[0].C;
+// document.getElementById('d').innerHTML=array[0].D;
+$(function(){
+    getJSON();
+})
+        
+function getJSON(){
+    $.getJSON('question.json',function(json){
+        questions = json;
+ // console.log(json);
+ 
+     localStorage.setItem('que', JSON.stringify(questions));
+     console.log(localStorage.getItem('que'));
+   }
+ )};
